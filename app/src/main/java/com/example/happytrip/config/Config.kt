2a8cfa35f783ac41ac57eff71a88c271aeb71f68
AufaAsmawy19551 <aufamuhammad19551@gmail.com>
2022-12-08 23:31:48 +0700

    add gitignore

--- app/src/main/java/com/example/happytrip/config/Config.kt
+++
@@ -1,9 +0,0 @@
-package com.example.happytrip.config
-
-import com.example.happytrip.restClient.traveler.response.auth.LoginResponse
-
-class Config {
-    companion object{
-        var baseURL = "http://192.168.42.11:8000/"
-    }
-}
